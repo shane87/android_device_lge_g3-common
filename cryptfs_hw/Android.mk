8a5fc66ffec275ecc23094f11aecb09902984dcc
Eric Clothier <eric.clothier@gmail.com>
2015-08-29 21:56:22 -0400

    Omnifying: add directive to copy cryptfs_hw.h to out dir for other modules

--- cryptfs_hw/Android.mk
+++ cryptfs_hw/Android.mk
@@ -12,6 +12,10 @@ commonSharedLibraries := \
                         libutils \
                         libdl
 
+# Export cryptfs_hw.h for other modules
+LOCAL_COPY_HEADERS_TO := libcryptfs_hw
+LOCAL_COPY_HEADERS    := cryptfs_hw.h
+
 LOCAL_C_INCLUDES := $(commonIncludes)
 LOCAL_SRC_FILES := $(sourceFiles)
 
